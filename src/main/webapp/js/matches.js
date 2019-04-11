321600ca7cb7e494616ccc7fdbaf275d6f9e5d92
yami <yami@yamis-air.fritz.box>
2019-04-11 09:01:00 +0200

    minor padding between individual results on matches page

--- src/main/webapp/js/matches.js
+++ src/main/webapp/js/matches.js
@@ -117,8 +117,13 @@
 	     $.each(data,function(){
 	     //if(!this.main_photo || this.main_photo == ''){ this.main_photo="/images/no_photo_available.png";}
 	     resultsHtml +=  "<div class=\"container\">"+
-	      "<div class=\"row\">"+
-	         
+	      "<div class=\"row\" style=\" margin-top: 50px;\">"+
+	          "<div class=\"col-sm-5\">"+
+	            "<div class=\"card\">"+
+	               "<img src=\""+this.main_photo+"\" alt=\"\">"+
+	               
+	            "</div>"+
+	         "</div>"+
 	         "<div class=\"col-sm-7\">"+
 	            "<div class=\"card\">"+
 	               
@@ -136,13 +141,13 @@
 	               "</div>"+
 	            "</div>"+
 	         "</div>"+
-	         "<div class=\"col-sm-5\">"+
-	            "<div class=\"card\">"+
-	               "<img src=\""+this.main_photo+"\" alt=\"\">"+
-	               
-	            "</div>"+
-	         "</div>"+
+	        
 	      "</div>"+
+	       "<div class=\"row\">"+
+	          "<div class=\"col-sm-12\">"+
+	            
+	          "</div>"+
+	       "</div>"+
       "</div>"
       
       
